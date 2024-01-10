2a4e4bb26aa0e215c3edf2bc10b3c1e4b4dde770
Jinsun Yoo <jsyoo1029@gmail.com>
2024-01-10 19:59:11 +0900

    Filter

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -4,6 +4,7 @@ def example_view(request):
     return render(request, 'my_app/example.html')
 
 def variable_view(request):
-    my_var = {'first_name': 'Rosalind', 'last_name': 'Franklin', 
+    my_var = {'first_name': 'rosaLind', 'last_name': 'franKlin', 
               'some_list': [1,2,3], 'some_dict': {'inside_key': 'inside_value'}}
     return render(request, 'my_app/variable.html', context = my_var)
+
