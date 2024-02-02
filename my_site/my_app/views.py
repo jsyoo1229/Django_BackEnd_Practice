16bff5dd53b9f1d7fbd4f5b75e65001778635f79
Jinsun Yoo <jsyoo1029@gmail.com>
2024-02-02 23:44:54 +0900

    If, Elif, Else

--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+
 def example_view(request):
-    return render(request, 'my_app/example.html')
+    return render(request, "my_app/example.html")
+
 
 def variable_view(request):
-    my_var = {'first_name': 'rosaLind', 'last_name': 'franKlin', 
-              'some_list': [1,2,3], 'some_dict': {'inside_key': 'inside_value'}}
-    return render(request, 'my_app/variable.html', context = my_var)
+    my_var = {
+        "first_name": "rosaLind",
+        "last_name": "franKlin",
+        "some_list": [1, 2, 3],
+        "user_logged_in": True,
+    }
 
+    return render(request, "my_app/variable.html", context=my_var)
